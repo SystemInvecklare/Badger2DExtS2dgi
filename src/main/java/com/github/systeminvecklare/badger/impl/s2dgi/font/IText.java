760f3c2907e8a83635449a0e96a8e2d1f8e1402c
Mattias Selin <mattias.selin@gmail.com>
2023-06-18 16:45:13 +0200

    Added convencience method

--- src/main/java/com/github/systeminvecklare/badger/impl/s2dgi/font/IText.java
+++ src/main/java/com/github/systeminvecklare/badger/impl/s2dgi/font/IText.java
@@ -1,5 +1,6 @@
 package com.github.systeminvecklare.badger.impl.s2dgi.font;
 
+import com.github.systeminvecklare.badger.core.widget.IRectangle;
 import com.github.systeminvecklare.badger.impl.s2dgi.drawcycle.S2dgiDrawCycle;
 
 public interface IText {
@@ -10,4 +11,11 @@ public interface IText {
 	int getYOffset();
 	int getWidth();
 	int getHeight();
+	
+	//Convenience method
+	default void renderIn(S2dgiDrawCycle drawCycle, IRectangle rectangle, float alignX, float alignY) {
+		int x = rectangle.getX() + (int) ((rectangle.getWidth() - this.getWidth())*alignX);
+		int y = rectangle.getY() + (int) ((rectangle.getHeight() - this.getHeight())*alignY);
+		this.render(drawCycle, x, y);
+	}
 }
